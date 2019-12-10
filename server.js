81e88aebf00327f86c8d03f39700c9b9592f98f9
rstowe1 <32300186+rstowe1@users.noreply.github.com>
2019-12-10 12:23:41 -0500

    Update server.js

--- server.js
+++ server.js
@@ -18,6 +18,16 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
+// Serve React in productoin
+if (process.env.NODE_ENV === "production") {
+  // Set Static Folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server Started on Port ${PORT}`));
